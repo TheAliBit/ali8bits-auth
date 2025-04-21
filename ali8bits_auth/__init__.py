e9891a888ef8c43a5f4cf79f3d27989f32d04808
TheAliBit <the.ali8bits@gmail.com>
2025-04-21 12:20:00 +0400

    feat: first commit & add basics

---
+++ ali8bits_auth/__init__.py
@@ -0,0 +1,8 @@
+"""
+ali8bits_auth.
+
+My python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Ali8Bits'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ali8bits_auth',
+    version='0.1.0',
+    description='An auth Python package that can be use in your django projects for authentication.',
+    url='https://github.com/shuds13/pyexample',
+    author='Ali8Bits',
+    author_email='the.ali8bits@gmail.com',
+    license='MIT License',
+    packages=['ali8bits_auth'],
+    install_requires=['mpi4py>=4.0.3',
+                      'numpy',
+                      ],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9.6'
+    ],
+)
